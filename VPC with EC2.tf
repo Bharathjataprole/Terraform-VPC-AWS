64122e9f483270dbe6c58b557cb7962a00500cf1
Bharathjataprole <jataprolebharath944@gmail.com>
2024-07-09 23:54:59 +0530

    Update and rename main.tf to VPC with EC2.tf

--- VPC with EC2.tf
+++ VPC with EC2.tf
@@ -6,7 +6,15 @@ provider "aws" {
 resource "aws_vpc" "demo-vpc" {
   cidr_block = "10.10.0.0/16"
 }
+# Create public subnet
+resource "aws_subnet" "demo-subnet" {
+  vpc_id     = aws_vpc.demo-vpc.id
+  cidr_block = "10.10.1.0/24"
 
+  tags = {
+    Name = "demo-subnet"
+  }
+}
 # Create internet gateway
 resource "aws_route_table" "demo-igw" {
   vpc_id = aws_vpc.demo-vpc.id
@@ -21,34 +29,23 @@ resource "aws_route_table" "demo-igw" {
   }
 }
 
-# Create public subnet
-resource "aws_subnet" "demo-subnet" {
-  vpc_id     = aws_vpc.demo-vpc.id
-  cidr_block = "10.10.1.0/24"
-
-  tags = {
-    Name = "demo-subnet"
-  }
-}
 
 # Create route table for public subnet
-resource "aws_route_table" "public_route_table" {
+resource "aws_route_table" "demo-rt" {
   vpc_id = aws_vpc.my_vpc.id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.demo-igw.id
   }
-
   tags = {
-    Name = "public_route_table"
+    Name = "demo-rt"
   }
 }
 
 # Associate public subnet with the public route table
 resource "aws_route_table_association" "demo-rt_assosiation" {
   subnet_id      = aws_subnet.demo-subnet.id
-  route_table_id = aws_route_table.public_route_table.id
+  route_table_id = aws_route_table.demo-rt.id
 }
 # Security group
 resource "aws_security_group" "demo-secgrp" {
@@ -57,16 +54,16 @@ resource "aws_security_group" "demo-secgrp" {
   vpc_id      = aws_vpc.demo-vpc.id
 }
 ingress {
-    description  = "TLS from VPC"
-    from port = 22
+    
+    from_port = 22
     to_port   = 22
     protocol  = "tcp"
-    cide_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
 }
 
 egress {
-  from port     = 0
+  from_port     = 0
   to_port       = 0
 protocol         = "-1"
 cidr_blocks     =  ["0.0.0.0/0"]
@@ -76,6 +73,7 @@ ipv6_cidr_blocks = ["::/0"]
 # Create EC2 instances
 resource "aws_instance" "demo-server" {
   ami           = "ami-04a81a99f5ec58529"
+  key_name = "jen-ter-private-kp"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.demo-subnet.id
   vpc_security_group_ids = [aws_security_group.demo-secgrp.id]
